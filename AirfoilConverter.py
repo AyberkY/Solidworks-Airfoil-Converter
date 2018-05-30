e40b1c7c7584a5a02fbc4d23fec42ee90b4405e4
Ayberk Yaraneri <35669067+AyberkY@users.noreply.github.com>
2018-05-31 00:24:10 +0300

    Converting algorithm revamp
    Took me about a year to learn to use .split() lol
    
    Much tidier now.

--- AirfoilConverter.py
+++ AirfoilConverter.py
@@ -5,12 +5,10 @@ Created on Sat Nov 18 13:22:13 2017
 @author: PC
 
 Expects a .dat type airfoil polar from airfoiltools.com
-Converts airfoil plot into one the SolidWorks can use.
-
-test change :D
+Converts airfoil plot into one that SolidWorks can use.
 """
 
-def addZero(origin, newfile):
+def Convert(origin, newfile):
     """
         input: an open file handle to read from (origin).
         output: an open file handle to write into (newfile).
@@ -18,27 +16,17 @@ def addZero(origin, newfile):
         Function turns spaces between each value in each line into tabs.
         Function then adds the value 0 for each line's Z axis.
     """
-    n = 0
-    tabFlag = False     #True if a tab was previously appended. Prevents multiple successive spaces being turned to multiple successive tabs.
-    for line in origin:
-        if n != 0:  #Does not add the first line, as it's usually the airfoil's name.
-            str1 = ""   #Temporary string
-            first = True
-            for char in line:
-                if char == "\n":    #If "carriage return", then:
-                    str1 = str1 + "\t0\n"   #Then tabs, adds a zero, then ends line.
-                    tabFlag = False
-                elif char == " ":
-                    if first == False:
-                        if tabFlag == False:    #Prevents multiple successive tabs.
-                            str1 = str1 + "\t"
-                            tabFlag = True
-                else:
-                    str1 = str1 + char
-                    tabFlag = False
-                    first = False
-            newfile.write(str1)
-        n += 1
+    points = origin.read().split('\n')
+
+    for point in points[1:]:
+        coordinates = point.split()
+        if len(coordinates):
+            newfile.write(str.format('{0:.6f}', float(coordinates[0])))
+            newfile.write('\t')
+            newfile.write(str.format('{0:.6f}', float(coordinates[1])))
+            newfile.write('\t')
+            newfile.write('0.000000')
+            newfile.write('\n')
     
     
 filename = input("File to convert?: " )
@@ -48,9 +36,9 @@ try:
     origin = open(filename, "r")
     newfile = open(newfilename, "w")
 except IOError:
-    print("Could not open file, please try again.")
+    print("Couldn't open file or files, try again.")
 else:
-    addZero(origin, newfile)
+    Convert(origin, newfile)
     print("Conversion succeeded.")
 finally:
     newfile.close()
