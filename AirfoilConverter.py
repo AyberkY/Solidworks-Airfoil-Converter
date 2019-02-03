3abb06612939d0b15d512aa75a889450fb2769cd
Ayberk Yaraneri <ayberkyaraneri@gmail.com>
2019-02-03 00:30:41 -0600

    Added scale functionality.

--- AirfoilConverter.py
+++ AirfoilConverter.py
@@ -8,11 +8,11 @@ Expects a .dat type airfoil polar from airfoiltools.com
 Converts airfoil plot into one that SolidWorks can use.
 """
 
-def Convert(origin, newfile):
+def Convert(origin, newfile, scaleFactor):
     """
         input: an open file handle to read from (origin).
         output: an open file handle to write into (newfile).
-        
+
         Function turns spaces between each value in each line into tabs.
         Function then adds the value 0 for each line's Z axis.
     """
@@ -21,14 +21,15 @@ def Convert(origin, newfile):
     for point in points[1:]:
         coordinates = point.split()
         if len(coordinates):
-            newfile.write(str.format('{0:.6f}', float(coordinates[0])))
+            newfile.write(str.format('{0:.6f}', float(coordinates[0])*scaleFactor))
             newfile.write('\t')
-            newfile.write(str.format('{0:.6f}', float(coordinates[1])))
+            newfile.write(str.format('{0:.6f}', float(coordinates[1])*scaleFactor))
             newfile.write('\t')
             newfile.write('0.000000')
             newfile.write('\n')
-    
+
 filename = input("File to convert?: " )
+scaleFactor = float(input("Scale?: "))
 newfilename = input("New file name?: ")
 
 try:
@@ -37,7 +38,7 @@ try:
 except IOError:
     print("Couldn't open file or files, try again.")
 else:
-    Convert(origin, newfile)
+    Convert(origin, newfile, scaleFactor)
     print("Conversion succeeded.")
 finally:
     newfile.close()
